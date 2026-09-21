1cc36f0fe51d6846a6dbf64812d8705621f57215
Mohd Firzan Bin Mohd Fauzi <firzan.fauzi@mediaprima.com.my>
2026-09-21 18:50:48 +0800

    point api to render backend

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
     proxy: {
-      '/api': 'http://localhost:3000'
+      '/api': 'https://my-first-app-lcw6.onrender.com'
     }
   }
 })
